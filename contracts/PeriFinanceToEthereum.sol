b5a3a5328004bb2f15384a5589314379ec4d9c05
syoonk <koseyo89@gmail.com>
2021-07-02 21:08:55 +0900

    Applying not implemented

--- contracts/PeriFinanceToEthereum.sol
+++ contracts/PeriFinanceToEthereum.sol
@@ -18,6 +18,88 @@ contract PeriFinanceToEthereum is PeriFinance {
         _notImplemented();
     }
 
+    function issuePynthsAndStakeUSDC(uint _issueAmount, uint _usdcStakeAmount) external {
+        _notImplemented();
+    }
+
+    function issueMaxPynths() external {
+        _notImplemented();
+    }
+
+    function issuePynthsAndStakeMaxUSDC(uint _issueAmount) external {
+        _notImplemented();
+    }
+
+    function burnPynthsAndUnstakeUSDC(uint _burnAmount, uint _unstakeAmount) external {
+        _notImplemented();
+    }
+
+    function burnPynthsAndUnstakeUSDCToTarget() external {
+        _notImplemented();
+    }
+
+    function exchange(
+        bytes32 sourceCurrencyKey,
+        uint sourceAmount,
+        bytes32 destinationCurrencyKey
+    ) external returns (uint amountReceived) {
+        _notImplemented();
+    }
+
+    function exchangeOnBehalf(
+        address exchangeForAddress,
+        bytes32 sourceCurrencyKey,
+        uint sourceAmount,
+        bytes32 destinationCurrencyKey
+    ) external returns (uint amountReceived) {
+        _notImplemented();
+    }
+
+    function exchangeWithTracking(
+        bytes32 sourceCurrencyKey,
+        uint sourceAmount,
+        bytes32 destinationCurrencyKey,
+        address originator,
+        bytes32 trackingCode
+    ) external returns (uint amountReceived) {
+        _notImplemented();
+    }
+
+    function exchangeOnBehalfWithTracking(
+        address exchangeForAddress,
+        bytes32 sourceCurrencyKey,
+        uint sourceAmount,
+        bytes32 destinationCurrencyKey,
+        address originator,
+        bytes32 trackingCode
+    ) external returns (uint amountReceived) {
+        _notImplemented();
+    }
+
+    function exchangeWithVirtual(
+        bytes32 sourceCurrencyKey,
+        uint sourceAmount,
+        bytes32 destinationCurrencyKey,
+        bytes32 trackingCode
+    ) external returns (uint amountReceived, IVirtualPynth vPynth) {
+        _notImplemented();
+    }
+
+    function settle(bytes32 currencyKey)
+        external
+        returns (
+            uint reclaimed,
+            uint refunded,
+            uint numEntries
+        )
+    {
+        _notImplemented();
+    }
+
+    function liquidateDelinquentAccount(address account, uint pusdAmount) external returns (bool) {
+        _notImplemented();
+    }
+
     function transfer(address to, uint value) external optionalProxy systemActive returns (bool) {
         _transferByProxy(messageSender, to, value);
     }
